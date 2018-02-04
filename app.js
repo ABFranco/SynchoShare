0e1ba82a9bd0b7d8be340ceedfe58c0ae27474a8
ABFranco <abfranco4@gmail.com>
2018-02-03 20:24:08 -0800

    added chat room

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ mongoose.connection.on('error', (err) => {
 })
 
 const app = express();
-//const port = process.env.PORT || 8080;
-const port = 3000;
+const port = process.env.PORT || 8080;
+//const port = 3000;
 
 
 const users = require('./routes/users');
