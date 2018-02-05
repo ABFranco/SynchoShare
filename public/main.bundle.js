1cc13274271c06d454e34dc55e7a08cbdfec00a7
ABFranco <abfranco4@gmail.com>
2018-02-05 13:29:23 -0800

    sockets working with music

--- public/main.bundle.js
+++ public/main.bundle.js
@@ -804,8 +804,8 @@ var RoomComponent = /** @class */ (function () {
     function RoomComponent(authService, router) {
         this.authService = authService;
         this.router = router;
-        //this.socket = socketIo('https://shrouded-badlands-38226.herokuapp.com/');
-        this.socket = __WEBPACK_IMPORTED_MODULE_3_socket_io_client__('localhost:3000');
+        this.socket = __WEBPACK_IMPORTED_MODULE_3_socket_io_client__('https://shrouded-badlands-38226.herokuapp.com/');
+        //this.socket = socketIo('localhost:3000');
         this.audio = null;
         this.playing = false;
     }
